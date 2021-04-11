07d1e32d18bcba89cf311e11edb795a5e88f469e
popdev86 <speedwobble100@gmail.com>
2021-04-11 17:27:30 -0400

    added note type to schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
-// index.js
-// This is the main entry point of application
+/*index.js
+ * Main entry point of program*/
 /*eslint-disable no-undef*/
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-
+/*port used for program*/
 const port = process.env.PORT || 4000;
-/*construct a schema using graphQL*/
+
+let notes = [
+  {
+    id: '1',
+    content: 'This is a note',
+    author: 'Mike Jones'
+  },
+  {
+    id: '2',
+    content: 'Another note',
+    author: 'Last Mr. Bigg'
+  },
+  {
+    id: '3',
+    content: 'Third note in object',
+    author: 'Birdman'
+  }
+];
+
+/*----------GraphQL----------*/
+/*Schemas for GraphQL*/
 const typeDefs = gql`
   type Query {
     hello: String
+    notes: [Note!]!
+  }
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
   }
 `;
-/*resolver function for above schema setup*/
+
+/*Resolvers for GraphQL*/
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
+    hello: () => 'Hello World!',
+    notes: () => notes
   }
 };
 
+/*----------Server----------*/
+/*store express in app variable*/
 const app = express();
 /*Apollo Server setup*/
 const server = new ApolloServer({ typeDefs, resolvers });
 /*apply the Apollo GraphQL middleware and set the path to /api*/
 server.applyMiddleware({ app, path: '/api' });
-
+/*port listener*/
 app.listen({ port }, () =>
   console.log(
     `GraphQL server  on http://localhost:${port}${server.graphqlPath}`
