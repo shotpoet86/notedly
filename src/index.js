f6fbe9342e07db47d8a91315e07c0ba8bd5a963a
popdev86 <speedwobble100@gmail.com>
2021-04-11 01:37:37 -0400

    connected graphQl  to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 // index.js
-// This is the main entry point of our application
-console.log('hello');
+// This is the main entry point of application
+/*eslint-disable no-undef*/
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+/*construct a schema using graphQL*/
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+/*resolver function for above schema setup*/
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!'
+  }
+};
+
+const app = express();
+/*Apollo Server setup*/
+const server = new ApolloServer({ typeDefs, resolvers });
+/*apply the Apollo GraphQL middleware and set the path to /api*/
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL server  on http://localhost:${port}${server.graphqlPath}`
+  )
+);
